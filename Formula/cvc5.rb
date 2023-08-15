94b4d6788af3f277412f53f7bd095509cdff59c1
Vinícius Camillo <viniciusrcamillo@gmail.com>
2023-08-15 07:07:52 -0300

    Reordering of dependencies.

--- Formula/cvc5.rb
+++ Formula/cvc5.rb
@@ -11,8 +11,8 @@ class Cvc5 < Formula
   option "with-python-bindings", "Build python bindings based on new C++ API"
 
   depends_on "cmake"
-  depends_on "python@3.10"
   depends_on "flex"
+  depends_on "python@3.10"
 
   resource "toml" do
     url "https://files.pythonhosted.org/packages/be/ba/1f744cdc819428fc6b5084ec34d9b30660f6f9daaf70eead706e3203ec3c/toml-0.10.2.tar.gz"
