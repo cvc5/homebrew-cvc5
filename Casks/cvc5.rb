c7e98a76cbc607b63525e01bfba0837a2e30749c
Daniel Larraz <daniel-larraz@users.noreply.github.com>
2025-05-06 15:18:59 -0500

    Add cask for cvc5 v1.2.1

---
+++ Casks/cvc5.rb
@@ -0,0 +1,25 @@
+cask "cvc5" do
+  arch arm: "arm64", intel: "x86_64"
+  
+  version "1.2.1"
+  sha256 arm:   "18e0bd283d44f720f72bf80175169ef63e985628b7ba1502aaf812f57f981461",
+         intel: "bdde9557bbb9b812af270c3f418836c285957b3ed81385b87428d2d595ffbf47"
+
+  url "https://github.com/cvc5/cvc5/releases/download/cvc5-#{version}/cvc5-macOS-#{arch}-static.zip"
+  name "cvc5"
+  desc "Automatic theorem prover for Satisfiability Modulo Theories (SMT) problems"
+  homepage "https://cvc5.github.io/"
+  
+  # Use GitHub releases to check for new versions
+  livecheck do
+    url :url
+    strategy :github_latest
+    regex(/^cvc5-(\d+(?:\.\d+)+)$/i)
+  end
+  
+  binary "cvc5-macOS-#{arch}-static/bin/cvc5"
+  
+  caveats do
+    license "https://github.com/cvc5/cvc5/blob/main/COPYING"
+  end
+end
