79f59ef0468d53db84b829f480a70c55911c4e50
Daniel Larraz <daniel-larraz@users.noreply.github.com>
2025-05-06 15:56:15 -0500

    Extend cask to support Linux

--- Casks/cvc5.rb
+++ Casks/cvc5.rb
@@ -2,11 +2,23 @@ cask "cvc5" do
   arch arm: "arm64", intel: "x86_64"
 
   version "1.2.1"
-  sha256 arm:   "18e0bd283d44f720f72bf80175169ef63e985628b7ba1502aaf812f57f981461",
-         intel: "bdde9557bbb9b812af270c3f418836c285957b3ed81385b87428d2d595ffbf47"
 
-  url "https://github.com/cvc5/cvc5/releases/download/cvc5-#{version}/cvc5-macOS-#{arch}-static.zip",
-      verified: "github.com/cvc5/cvc5/releases/download/"
+  on_macos do
+    url "https://github.com/cvc5/cvc5/releases/download/cvc5-#{version}/cvc5-macOS-#{arch}-static.zip",
+        verified: "github.com/cvc5/cvc5/releases/download/"
+    sha256 arm:   "18e0bd283d44f720f72bf80175169ef63e985628b7ba1502aaf812f57f981461",
+           intel: "bdde9557bbb9b812af270c3f418836c285957b3ed81385b87428d2d595ffbf47"
+    binary "cvc5-macOS-#{arch}-static/bin/cvc5"
+  end
+
+  on_linux do
+    url "https://github.com/cvc5/cvc5/releases/download/cvc5-#{version}/cvc5-Linux-#{arch}-static.zip",
+        verified: "github.com/cvc5/cvc5/releases/download/"
+    sha256 arm:   "de27e95e00d16fbd7697ab7eac3046d0a11ca8753e2f50c5ed52604e75691291",
+           intel: "6d44abc233980a14d72cc5809287d27c3335b1d6ee863381d0b5ffcbd0d8de56"
+    binary "cvc5-Linux-#{arch}-static/bin/cvc5"
+  end
+
   name "cvc5"
   desc "Automatic theorem prover for Satisfiability Modulo Theories (SMT) problems"
   homepage "https://cvc5.github.io/"
@@ -17,8 +29,6 @@ cask "cvc5" do
     strategy :github_latest
     regex(/^cvc5-(\d+(?:\.\d+)+)$/i)
   end
-
-  binary "cvc5-macOS-#{arch}-static/bin/cvc5"
 
   caveats do
     license "https://github.com/cvc5/cvc5/blob/main/COPYING"
