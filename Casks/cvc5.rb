cf0714bf14dc6969bdcf33126a0ec6e91992426a
Daniel Larraz <daniel-larraz@users.noreply.github.com>
2025-12-16 11:18:44 -0600

    Update cvc5 cask to 1.3.2 (#21)

--- Casks/cvc5.rb
+++ Casks/cvc5.rb
@@ -1,19 +1,19 @@
 cask "cvc5" do
-  version "1.3.1"
+  version "1.3.2"
 
   on_macos do
     arch arm: "arm64", intel: "x86_64"
     url "https://github.com/cvc5/cvc5/releases/download/cvc5-#{version}/cvc5-macOS-#{arch}-static.zip",
         verified: "github.com/cvc5/cvc5/releases/download/"
-    sha256 arm:   "a0e7f5b03b1bc4284fbfff7cdfb08c704801701cf7ece83a13f8a505e7581215",
-           intel: "e7fe4af9491bd7c0db7591c0a483775735bd1a98b23933fd337a73ae39c10ff9"
+    sha256 arm:   "172b6ff70662184725aedf64b0189a870cc7562aca1bad9cd0ec92f682edb3af",
+           intel: "b4ab528a63592da89c81eb10e35167f1e6051fd2ad8969f4e6ec54e0708fe774"
     binary "cvc5-macOS-#{arch}-static/bin/cvc5"
   end
 
   on_linux do
     url "https://github.com/cvc5/cvc5/releases/download/cvc5-#{version}/cvc5-Linux-x86_64-static.zip",
         verified: "github.com/cvc5/cvc5/releases/download/"
-    sha256 "1a1cda20d2df4938fa4944a69f33ddc9172e319ece0eed0aa09c4d7abede3ed1"
+    sha256 "1060daaf507edef9d0a68e399cfc0e9038150bccb9e2d34d081d50a7687544d2"
     binary "cvc5-Linux-x86_64-static/bin/cvc5"
   end
 
